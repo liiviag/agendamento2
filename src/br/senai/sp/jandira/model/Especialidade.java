7310c7237fd220759e5bd6c29fb48fd7354dad8b
Ana Livia <liiviggularte@gmail.com>
2022-09-29 17:01:12 -0300

    att Netbeans

--- src/br/senai/sp/jandira/model/Especialidade.java
+++ src/br/senai/sp/jandira/model/Especialidade.java
@@ -4,48 +4,50 @@ import javax.swing.JOptionPane;
 
 public class Especialidade {
 
-	private String nome;
-	private String descricao;
-	
-	//Construtores da classe
-	
-	public Especialidade(String nome) {
-		this.nome = nome;
-	}
-	
-	
-	public Especialidade(String nome, String descricao) {
-		this.nome = nome;
-		this.descricao = descricao;
-	}
-	
-	public Especialidade() {
-		
-	}
-	// Métodos de acessos aos atributos
-	
-		public void setNome (String nome) {
-			
-			if (nome.length() >= 3) {
-				this.nome = nome;
-			}else {
-				JOptionPane.showMessageDialog(null, nome + " não é um nome válido\nDeve conter pelo menos 3 LETRAS!!");
-			}
-			
-		}
-			public String getNome() {
-				return nome;
-			}
-		public void setDescricao (String descricao) {
-			
-			if (descricao.length() >= 10) {
-				this.descricao = descricao;
-			}else {
-				JOptionPane.showMessageDialog(null, descricao + " não é uma descrição válida\nDeve conter pelo menos 10 LETRAS!!");
-			}
-		}
-			public String getDescricao() {
-				return this.descricao;
-			}
-	
+    private Integer codigo;
+    private String nome;
+    private String descricao;
+
+    //Construtores da classe
+    public Especialidade(String nome) {
+        this.nome = nome;
+    }
+
+    public Especialidade(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Especialidade() { //Construtor Default / Padrão 
+
+    }
+
+    // Métodos de acessos aos atributos
+    public void setNome(String nome) {
+
+        if (nome.length() >= 3) {
+            this.nome = nome;
+        } else {
+            JOptionPane.showMessageDialog(null, nome + " não é um nome válido\nDeve conter pelo menos 3 LETRAS!!");
+        }
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setDescricao(String descricao) {
+
+        if (descricao.length() >= 10) {
+            this.descricao = descricao;
+        } else {
+            JOptionPane.showMessageDialog(null, descricao + " não é uma descrição válida\nDeve conter pelo menos 10 LETRAS!!");
+        }
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
 }
